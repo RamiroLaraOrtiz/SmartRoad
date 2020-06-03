042ff1c0b13f4d330829712d3fe6fcde6f468489
omarbarbosa06 <omarbarbosa06@gmail.com>
2020-06-03 17:36:18 -0500

    Cambio historial de infraccion

--- src/app/historial/historial.page.ts
+++ src/app/historial/historial.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import data from '../../assets/icon/warnings.json'
+import data from '../../assets/icon/warnings.json';
+import { AlertController } from "@ionic/angular";
+import { groupRowsByParents } from '@swimlane/ngx-datatable';
 @Component({
   selector: 'app-historial',
   templateUrl: './historial.page.html',
@@ -8,14 +10,29 @@ import data from '../../assets/icon/warnings.json'
 export class HistorialPage implements OnInit {
   private insfracciones = data;
   tableStyle = 'boostrap';
-  constructor() { 
+  constructor(private AlertController: AlertController) { 
     console.log(this.insfracciones);
   }
 
-  async open(row){
-    console.log(row);
+  async open(fecha, speedlimit, yourspeedlimit, coordinates, lugar){
+    const alertElement =await this.AlertController.create({
+      header:'Fecha: '+fecha,
+      message: 'Lugar: '+lugar+'<br><br>'+'Limite de velocidad: '+speedlimit+'<br><br>'+'Velocidad a la que ibas: '+yourspeedlimit+'<br><br>'+'En las coordenadas: '+coordinates,
+      buttons:[
+        {
+          text:'Ok!',
+          role:'cancel'
+        }
+      ]
+    })
+
+    await alertElement.present();
   }
 
+  async deletePlace(){
+    
+
+  }
   ngOnInit() {
   }
 
